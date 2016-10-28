408ee29fe5bf0d6e7a735492174592a9f9c4e05b
Nelson Páez <nelpa90@gmail.com>
2016-10-27 22:44:01 -0300

    Commit inicial

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name minihubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the minihubApp
+ */
+angular.module('minihubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
